daa0cfaa0be62ffc6224c57b8764ad5a8cc3b05e
Дмитрий <mr.gn0m@yandex.ru>
2026-02-10 18:36:10 +0300

    add infra for practic

---
+++ random/.terraform.lock.hcl
@@ -0,0 +1,9 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/random" {
+  version = "3.8.1"
+  hashes = [
+    "h1:Eexl06+6J+s75uD46+WnZtpJZYRVUMB0AiuPBifK6Jc=",
+  ]
+}
---
+++ random/main.tf
@@ -0,0 +1,21 @@
+resource "random_pet" "server" {
+  length = 2
+}
+
+resource "random_integer" "port" {
+  min = 1000
+  max = 9999
+}
+
+resource "random_string" "password" {
+  length  = 16
+  special = true
+}
+
+output "server_name" {
+  value = "web-${random_pet.server.id}"
+}
+
+output "server_port" {
+  value = random_integer.port.result
+}
